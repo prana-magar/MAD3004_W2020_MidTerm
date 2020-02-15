6db4e2a9043984588d1502a7ce3809bd72a26620
prana <prakashrana9847067614@gmail.com>
2020-02-14 19:41:18 -0500

    Added customer Class structure

---
+++ MAD2020/MAD2020/Customer.swift
@@ -0,0 +1,18 @@
+//
+//  Customer.swift
+//  MAD2020
+//
+//  Created by Prakash on 14/02/20.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+class Customer: Display, CustomStringConvertible {
+    
+    
+    
+    func display() {
+        print(self.description)
+    }
+}
