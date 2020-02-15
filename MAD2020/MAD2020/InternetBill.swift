5de55d0da61a6719b913fd9de5a0810c5337e609
prana <prakashrana9847067614@gmail.com>
2020-02-14 19:31:24 -0500

    Added Intetrnet bill class

---
+++ MAD2020/MAD2020/InternetBill.swift
@@ -0,0 +1,27 @@
+//
+//  InternetBill.swift
+//  MAD2020
+//
+//  Created by Prakash on 14/02/20.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+class InternetBill: Bill {
+    var id: String
+    var date: Date
+    var type: BillType = BillType.Internet
+    var total: Float = 0
+    var providerName: String
+    var usedGB: Float
+    
+    init(id: String, date: Date, providerName: String, usedGB: Float) {
+        self.id = id
+        self.date = date
+        self.providerName = providerName
+        self.usedGB = usedGB
+    }
+
+    
+}
