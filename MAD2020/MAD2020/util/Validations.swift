2cda6ad78cffa9ecf56072d576d71faec49f2f78
prana <prakashrana9847067614@gmail.com>
2020-02-18 15:11:52 -0500

    added validations

---
+++ MAD2020/MAD2020/util/Validations.swift
@@ -0,0 +1,54 @@
+//
+//  EmailChecker.swift
+//  MAD2020
+//
+//  Created by Prakash on 2020-02-18.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+enum EmailValidationError: Error{
+    case isNotValidEmail
+    case isEmpty
+    case isNotValidLength
+}
+
+enum PhoneNumberValidationError: Error{
+    case voiletsMinLength
+    case voiletsMaxLength
+}
+
+struct Validations {
+    private static let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    static func email(email: String) throws {
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+        if !emailPred.evaluate(with: email){
+            throw EmailValidationError.isNotValidEmail
+        }
+        
+        if email.isEmpty{
+            throw EmailValidationError.isEmpty
+        }
+        
+        if email.count > 254
+        {
+            throw EmailValidationError.isNotValidLength
+        }
+    }
+    
+    
+    static func mobileNumber(number: String) throws {
+        if number.count < 7 {
+            throw PhoneNumberValidationError.voiletsMinLength
+        }
+        
+        if number.count > 16 {
+            throw PhoneNumberValidationError.voiletsMaxLength
+        }
+    }
+    
+    
+}
