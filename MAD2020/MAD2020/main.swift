e3caafdc4a5df1853cf0af6fbe64fb418aa289b9
prana <prakashrana9847067614@gmail.com>
2020-02-17 20:40:54 -0500

    added bills in customer class

--- MAD2020/MAD2020/main.swift
+++ MAD2020/MAD2020/main.swift
@@ -8,20 +8,24 @@
 
 import Foundation
 
+
+var cc = Customer(id: "45", firstName: "Ram", lastName: "rr", email: "sdas@gmail.com")
+
+
+
 var bb:Bill
 bb = MobileBill(id: "1", date: Date(), type: BillType.Hydro, modelName: "Apple", number: 123123, usedGB: 5.6, usedMinutes: 56)
 
-
+cc.addBill(bill: bb)
 print(bb)
 
 bb = InternetBill(id: "2", date: Date(), providerName: "Fido", usedGB: 54.33)
 print(bb)
-
+cc.addBill(bill: bb)
 
 bb = HydroBill(id: "4", date: Date(), agencyName: "TORONTOH", unitConsumed: 44.9)
 print(bb)
+cc.addBill(bill: bb)
 
-
-var cc = Customer(id: "45", firstName: "Ram", lastName: "rr", email: "sdas@gmail.com")
 
 print(cc)
